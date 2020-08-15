4e3dacc9f732812a3541442e1d76dfacc10d0205
ninjyasoppy <69029725+ninjyasoppy@users.noreply.github.com>
2020-08-15 20:11:14 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
  root to: 'posts#index'
-# get 'posts/new', to: 'posts#new'←これは削除する
  post 'posts', to: 'posts#create'
+ get 'posts', to: 'posts#checked'#メモのidを取得できるようにルーティングに設定
 end
